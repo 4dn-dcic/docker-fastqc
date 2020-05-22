c295aca5b6e199b3dcec4a2ef36ce7c8a4de5bed
Soo Lee <duplexa@gmail.com>
2020-05-22 15:00:07 -0400

    Update tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,9 +3,21 @@
 image_name=docker-fastqc_local  # local image name for testing
 
 cwd=$(pwd)
+
+# non=gz input
 mkdir -p tmpout
-docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name sh -c 'run.sh /d/somefile.gz 1 /e/'
+docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name sh -c 'run.sh /d/somefastq.fq 1 /e/'
 if [ -z "$(ls -1 tmp_out/)"]; then
   echo "test failed - no output found"
   return 1;
 fi
+rm -rf tmpout  # cleanup
+
+# gz input
+mkdir -p tmpout
+docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name sh -c 'run.sh /d/somefastq.fq.gz 1 /e/'
+if [ -z "$(ls -1 tmp_out/)"]; then
+  echo "test failed - no output found"
+  return 1;
+fi
+rm -rf tmpout  # cleanup
