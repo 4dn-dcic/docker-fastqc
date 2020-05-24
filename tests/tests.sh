b6bc9f6b1f36bcc7718caf104b8dce5aac14fe8b
Soo Lee <duplexa@gmail.com>
2020-05-23 22:38:12 -0400

    Update tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,7 +7,7 @@ cwd=$(pwd)
 # non=gz input
 mkdir -p tmpout
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name sh -c 'run.sh /d/somefastq.fq 1 /e/'
-if [ -z "$(ls -1 tmp_out/)"]; then
+if [ -z "$(ls -1 tmp_out/)" ]; then
   echo "test failed - no output found"
   return 1;
 fi
@@ -16,7 +16,7 @@ rm -rf tmpout  # cleanup
 # gz input
 mkdir -p tmpout
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name sh -c 'run.sh /d/somefastq.fq.gz 1 /e/'
-if [ -z "$(ls -1 tmp_out/)"]; then
+if [ -z "$(ls -1 tmp_out/)" ]; then
   echo "test failed - no output found"
   return 1;
 fi
