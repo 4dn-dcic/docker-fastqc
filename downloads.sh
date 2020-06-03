8af5a91f3804191996327d0a7e1482696b553a8c
Soo Lee <duplexa@gmail.com>
2020-06-03 11:50:39 -0400

    Create downloads.sh

---
+++ downloads.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## SOFTWARE: fastqc
+## VERSION: 0.11.5
+wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.zip
+unzip fastqc_v0.11.5.zip
+cd FastQC/
+chmod 755 fastqc
+cd ..
